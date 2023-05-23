0e51a232d9399cc4e7e4731b82e1b9ea702a0e40
SalamAdil2442 <salamadilsaidan@gmail.com>
2023-05-23 23:44:14 +0300

    Add project files.

---
+++ Crud API without database/Controllers/UserAPIController.cs
@@ -0,0 +1,87 @@
+﻿using Crud_API_without_database.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crud_API_without_database.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAPIController : ControllerBase
+         
+    {
+        private static List<Users> callusers = new List<Users>
+        {  
+        new Users
+            {
+            ID=1,
+            Name="Salam",
+            Email="Salamadilsaidan@gmail.com",
+            GitHub="SalamAdil2442",
+            Password=1234567890,
+            Place="Erbil"
+            },       new Users
+            {
+            ID=2,
+            Name="ahmad",
+            Email="ahmadadil@gmail.com",
+            GitHub="username",
+            Password=1234567890,
+            Place="erbil"
+            }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Users>>> get()
+        {
+            return Ok(callusers);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Users>>> get( int id)
+        {
+            var usersID = callusers.Find(h=> h.ID==id);
+            if (usersID == null)
+            {
+                return BadRequest("NOT FOUND");
+            }
+            return Ok(usersID);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<Users>>> post(Users adduser)
+        {
+            callusers.Add(adduser);
+            return Ok(callusers);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Users>>> Update(Users edit)
+        {
+            var useredit=callusers.Find(h => h.ID==edit.ID);
+            if (useredit == null)
+            {
+                return BadRequest("NOT FOUND");
+
+                
+            }
+            useredit.ID = edit.ID;
+         useredit.Name=edit.Name;
+            useredit.Password=edit.Password;
+            useredit.Place=edit.Place;
+            useredit.Email=edit.Email;
+            useredit.GitHub=edit.GitHub;
+            return Ok(useredit);
+         
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Users>>> delete(int id) {
+            var deletuser = callusers.Find(h => h.ID == id)
+                    ;
+            if(deletuser == null)
+            {
+                return BadRequest("NOT FOUND");
+            }
+            callusers.Remove(deletuser);
+            return Ok(deletuser);
+                }
+
+    }
+}
---
+++ Crud API without database/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿namespace Crud_API_without_database.Models
+{
+    public class Users
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; }= string.Empty;
+        public double Password { get; set; }
+
+        public string Place { get; set; } = string.Empty;
+        public string GitHub { get; set; }  =string .Empty;
+
+
+
+
+
+
+    }
+}
